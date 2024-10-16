6b4aa8cf3b8b5a48a0518eff66613f0936a9d28f
Levy Wechuli <levy.wechuli@student.moringaschool.com>
2024-10-17 02:49:47 +0300

    added event listners to next previous and finish button

--- index.js
+++ index.js
@@ -1,90 +1,137 @@
-document.addEventListener("DOMContentLoaded", ()=>{
-const totalTime = 400;
-let timeRemaining = totalTime
-let score = 0;
-let questions = [];
+document.addEventListener("DOMContentLoaded", () => {
+  const totalTime = 400;
+  let timeRemaining = totalTime;
+  let score = 0;
+  let questions = [];
 
+  // getting elements from my html
+  const startBtn = document.getElementById("start-btn");
+  const questionContainer = document.getElementById("question-container");
+  const optionContainer = document.getElementById("options-container");
+  const nextBtn = document.getElementById("next-btn");
+  const prevBtn = document.getElementById("prev-btn");
+  const timeDisplay = document.getElementById("time");
+  const startContainer = document.getElementById("start-container");
 
-// getting elements from my html
-const startBtn = document.getElementById("start-btn");
-const questionContainer = document.getElementById("question-container");
-const optionContainer = document.getElementById("options-container");   
-const nextBtn = document.getElementById("next-btn");
-const prevBtn= document.getElementById("prev-btn");
-const timeDisplay = document.getElementById("time");
-const startContainer = document.getElementById("start-container");
+  //creating a finish button
+  const finishBtn = document.createElement("button");
+  finishBtn.id = "finish-btn";
+  finishBtn.innerText = "Finish";
+  questionContainer.appendChild(finishBtn);
 
-//creating a finish button
-const finishBtn = document.createElement("button");
-finishBtn.id = "finish-btn";
-finishBtn.innerText = "Finish";
-questionContainer.appendChild(finishBtn);
-
-startBtn.addEventListener('click', ()=>{
+  startBtn.addEventListener("click", () => {
     startContainer.style.display = "none";
-    questionContainer.style.display = 'block';
+    questionContainer.style.display = "block";
     optionContainer.style.display = "block";
     timeDisplay.style.display = "block";
     startTimer(totalTime);
-    fetchQuestions()
+    fetchQuestions();
+  });
 
-});
-
-function fetchQuestions(){
-   fetch("http://localhost:3000/questions") 
-   .then(Response =>Response.json())
-   .then(data => {
-       questions=data;
-       displayQuestion();//display my first question
-   })
-   .catch(error =>{
-    console.error('Error fetching questions:', error);
-   });
-}
- function displayQuestion(){
+  function fetchQuestions() {
+    fetch("http://localhost:3000/questions")
+      .then((Response) => Response.json())
+      .then((data) => {
+        questions = data;
+        displayQuestion(); //display my first question
+      })
+      .catch((error) => {
+        console.error("Error fetching questions:", error);
+      });
+  }
+  function displayQuestion() {
     const question = questions[currentQuestionIndex];
     questionContainer.innerText = question.question;
-    optionContainer.innerHTML="";
+    optionContainer.innerHTML = "";
 
     //my radio button options
-    question.options.forEach(option, index => {
-        const radioBtn = document.createElement("input");
-        radioBtn.type = "radio";
-        radioBtn.name = "option";
-        radioBtn.value= option;
-        radioBtn.id = 'option-${index}';
+    question.options.forEach(option, (index) => {
+      const radioBtn = document.createElement("input");
+      radioBtn.type = "radio";
+      radioBtn.name = "option";
+      radioBtn.value = option;
+      radioBtn.id = "option-${index}";
 
+      const label = document.createElement("label");
+      label.htmlFor = "option-${index}";
+      label.innerText = option;
 
-        const label = document.createElement("label");
-        label.htmlFor = "option-${index}";
-        label.innerText = option;
+      const br = document.createElement("br");
 
-        const br = document.createElement("br");
-        
-        optionContainer.appendChild(radioBtn);
-        optionContainer.appendChild(label);
-        optionContainer.appendChild(br);
-    
-    
+      optionContainer.appendChild(radioBtn);
+      optionContainer.appendChild(label);
+      optionContainer.appendChild(br);
     });
 
     prevBtn.style.display = currentQuestionIndex === 0 ? "none" : "inline";
 
     //we will have to show the next button if not the last question
-    if (currentQuestionIndex === questions.length -1){
-        nextBtn.style.display = "none";
-        finishBtn.style.display = "inline";
-    }else  {
-            nextBtn.style.display = "inline";
-            finishBtn.style.display = "none";
-    }   
- }  
- const selectedAnswer = selectedOption.value;
- checkAnswer ()
+    if (currentQuestionIndex === questions.length - 1) {
+      nextBtn.style.display = "none";
+      finishBtn.style.display = "inline";
+    } else {
+      nextBtn.style.display = "inline";
+      finishBtn.style.display = "none";
+    }
+  }
 
+  nextBtn.addEventListener("click", () => {
+    const selectedOption = document.querySelector('input[name="option"]:checked');
 
+    const selectedAnswer = selectedOption.value;
+    checkAnswer(selectedAnswer, questions[currentQuestionIndex].answer);
+
+    if (currentQuestionIndex < questions.length - 1) {
+      currentQuestionIndex++;
+      displayQuestion();
+    }
+  });
+
+  //previous button
+
+  prevBtn.addEventListener("click", ()=> {
+
+    if( currentQuestionIndex > 0){
+        currentQuestionIndex--;
+        displayQuestion();
+    }
+  });
+
+  //finish button
+  finishBtn.addEventListener("click", () => {
+    const selectedOption = document.querySelector('input[name="option"]:checked');
+    
+    const selectedAnswer =selectedOption.value;
+    checkAnswer(selectedAnswer, questions[currentQuestionIndex].answer);
+    endQuiz();
+
+  });
+
+  function checkAnswer(selectedAnswer, correct){
+    if(selected === correct){
+        score++;
+    }
+  }
+ function startTimer(duration){
+    timeRemaining = duration;
+    timeDisplay.innerText = timeRemaining;
+
+    timerInterval = setInterval(()=>{
+        timeRemaining--;
+        timeDisplay.innerText = timeRemaining;
+
+        if (timeRemaining <=0){
+            clearInterval(timerInterval);
+            endQuiz();
+        }
+    }, 1000);
+ }
 
  
+
+
+
+
 
 
 
