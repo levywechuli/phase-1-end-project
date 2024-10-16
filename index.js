d49a506a2045f41be99dc70b99c8a5bbf4e4a92a
Levy Wechuli <levy.wechuli@student.moringaschool.com>
2024-10-17 01:00:23 +0300

    working on the start buitton

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 document.addEventListener("DOMContentLoaded", ()=>{
+const totalTime = 400;
+let timeRemaining = totalTime
+let score = 0;
+let questions = [];
+
 
 // getting elements from my html
 const startBtn = document.getElementById("start-btn");
@@ -7,7 +12,28 @@ const optionContainer = document.getElementById("options-container");
 const nextBtn = document.getElementById("next-btn");
 const prevBtn= document.getElementById("prev-btn");
 const timeDisplay = document.getElementById("time");
-const startContainer = document.getElementById("start-container")
+const startContainer = document.getElementById("start-container");
+
+//creating a finish button
+const finishBtn = document.createElement("button");
+finishBtn.id = "finish-btn";
+finishBtn.innerText = "Finish";
+questionContainer.appendChild(finishBtn);
+
+startBtn.addEventListener('click', ()=>{
+    startContainer.style.display = "none";
+    questionContainer.style.display = 'block';
+    optionContainer.style.display = "block";
+    timeDisplay.style.display = "block";
+    startTimer(totalTime);
+    fetchQuestions()
+
+});
+
+function fetchQuestions(){
+    
+}
+
 
 
 
