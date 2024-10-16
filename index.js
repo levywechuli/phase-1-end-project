4dfbc6b9654e17dd5959331c961e81a6085b2cf8
Levy Wechuli <levy.wechuli@student.moringaschool.com>
2024-10-17 00:25:26 +0300

    getting my elements from html

---
+++ index.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", ()=>{
+
+// getting elements from my html
+const startBtn = document.getElementById("start-btn");
+const questionContainer = document.getElementById("question-container");
+const optionContainer = document.getElementById("options-container");   
+const nextBtn = document.getElementById("next-btn");
+const prevBtn= document.getElementById("prev-btn");
+const timeDisplay = document.getElementById("time");
+const startContainer = document.getElementById("start-container")
+
+
+
+});
